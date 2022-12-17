bf0196604f90c7e0817713eeb770e83e19753e2c
Алиев Расим Рауфович <1arr@spbt.local>
2022-12-17 10:19:24 +0300

    точки с запятой и ретурн

--- strange_bot_c_plus/main.cpp
+++ strange_bot_c_plus/main.cpp
@@ -13,20 +13,21 @@ int main
     string botName := "Your Brains";
     string userName, dayOfWeek;
 
-    сout << "Hello. I am " << botName << endl
-    сout << "What is your name?" << endl
-    getline(cin; userName)
-    cout << "Hello " << userName << endl
-    cout << "What is weekday now?" << endl
+    сout << "Hello. I am " << botName << endl;
+    сout << "What is your name?" << endl;
+    getline(cin; userName);
+    cout << "Hello " << userName << endl;
+    cout << "What is weekday now?" << endl;
     getline(cin; dayOfWeek);
     if(dayOfWeek = "monday") {
         cout << "Sunday is so far away!\n";
-    }
+    };
     elseif(dayOfWeek = "friday") {
         cout < "Hurraaa!\n";
-    }
+    };
     else {
         cout < "Working day ... Hrr\n";
-    }
-    retyrn 0;
+    }; 
+	// точки с запятой
+    return 0; //вместо retyrn return
 }
