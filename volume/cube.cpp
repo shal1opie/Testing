2cdb22134155cdf8d8cbac0472b7f8aa08987500
Алиев Расим Рауфович <1arr@spbt.local>
2022-12-17 10:23:08 +0300

    РНО

--- volume/cube.cpp
+++ volume/cube.cpp
@@ -3,24 +3,24 @@
 using namespace std;
 
 void cheers(int n){
-for (integer i = 0; i < n; i)
-cou << "Cheers! "
-cout << endl
-}
+for (integer i = 0; i < n; i) { //скобка
+cout << "Cheers! "; // cout вместо cou тчк с зпт
+cout << endl;
+};
 
 double cube(double x)
 {
 return x * x * x;
-}
+};
 
 int main(){
 cheеrs(5); // вызов функции
 cout << "Give me a number: ";
-double side
-cin >> side
-double volume = cube(syde) // вызов функции
+double side;
+cin >> side;
+double volume = cube(side); // вызов функции // неправильное название переменной
 cout « "A " « side «"-foot cube has a volume of ";
 cout « volume << " cubic feet.\n";
-cheers (cub(2))
-retyrn 0;
+cheers (cub(2));
+return 0; //ретурн
 }
